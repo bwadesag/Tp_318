618334de7c8806a3b759e8b2500bba6cb7d7d1ca
bwadesag <tallabaudoin@gmail.com>
2025-07-01 11:11:10 +0200

    creation_api

--- src/main/java/com/example/tp318/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.tp318.repository;
-
-import com.example.tp318.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, String> {
-} 
