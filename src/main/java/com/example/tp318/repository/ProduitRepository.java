74b950a9b3022adaaed667f5d7eef1971e5c277c
estele <estelengnemie@gmail.com>
2025-07-01 10:44:24 +0100

    creation des controller

--- src/main/java/com/example/tp318/repository/ProduitRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.tp318.repository;
-
-import com.example.tp318.model.Produit;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface ProduitRepository extends JpaRepository<Produit, UUID> {
-} 
